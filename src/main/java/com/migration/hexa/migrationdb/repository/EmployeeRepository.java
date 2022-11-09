0dc2ae14ae54a75dacf7a19650c18b8d9a629474
Guillermo Estrada <memo.capricornio@gmail.com>
2022-11-09 14:39:48 -0500

    Add application.yml

--- src/main/java/com/migration/hexa/migrationdb/repository/EmployeeRepository.java
+++ src/main/java/com/migration/hexa/migrationdb/repository/EmployeeRepository.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
